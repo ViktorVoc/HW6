b8e649e00af58bdea890475449590bbef75ab916
ViktorVoc <115698348+ViktorVoc@users.noreply.github.com>
2023-06-29 13:03:14 +0200

    Update hw6.py 123

--- hw6.py
+++ hw6.py
@@ -1,5 +1,8 @@
 import os
 import shutil
+import argparse
+import zipfile
+import tarfile
 
 def normalize(name):
     trans_dict = {
@@ -18,14 +21,12 @@ def normalize(name):
     return normalized_name
 
 def sort_folder(path):
-   
     os.makedirs(os.path.join(path, 'images'), exist_ok=True)
     os.makedirs(os.path.join(path, 'documents'), exist_ok=True)
     os.makedirs(os.path.join(path, 'audio'), exist_ok=True)
     os.makedirs(os.path.join(path, 'video'), exist_ok=True)
     os.makedirs(os.path.join(path, 'archives'), exist_ok=True)
 
-    
     for item in os.listdir(path):
         item_path = os.path.join(path, item)
 
@@ -33,10 +34,8 @@ def sort_folder(path):
             sort_folder(item_path)
             continue
 
-        
         extension = item.split('.')[-1].upper()
 
-        
         if extension in ['JPEG', 'PNG', 'JPG', 'SVG']:
             shutil.move(item_path, os.path.join(path, 'images', normalize(item)))
         elif extension in ['AVI', 'MP4', 'MOV', 'MKV']:
@@ -44,4 +43,28 @@ def sort_folder(path):
         elif extension in ['DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX']:
             shutil.move(item_path, os.path.join(path, 'documents', normalize(item)))
         elif extension in ['MP3', 'OGG', 'WAV', 'AMR']:
-            shutil.move(item)
+            shutil.move(item_path, os.path.join(path, 'audio', normalize(item)))
+        elif extension in ['ZIP', 'RAR', 'TAR', 'GZ']:
+            extract_archive(item_path, os.path.join(path, 'archives'))
+
+def extract_archive(archive_path, extract_path):
+    extension = os.path.splitext(archive_path)[1].lower()
+
+    if extension == '.zip':
+        with zipfile.ZipFile(archive_path, 'r') as zip_ref:
+            zip_ref.extractall(extract_path)
+    elif extension in ['.tar', '.tar.gz', '.tgz']:
+        with tarfile.open(archive_path, 'r:*') as tar_ref:
+            tar_ref.extractall(extract_path)
+
+def main():
+    parser = argparse.ArgumentParser(description='Sort files in a directory by type.')
+    parser.add_argument('directory', type=str, help='Path to the directory to sort')
+
+    args = parser.parse_args()
+    directory = args.directory
+
+    sort_folder(directory)
+
+if __name__ == '__main__':
+    main()
